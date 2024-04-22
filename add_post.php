f3781850268bdc083ea88a5b3770b18c8ab86f15
Achouak Cherif <a.cherif@esi-sba.dz>
2024-04-22 04:33:41 +0200

    Some chnages

--- add_post.php
+++ add_post.php
@@ -33,7 +33,7 @@ if (isset($_POST['publish'])) {
    if (isset($image)) {
       if ($select_image->rowCount() > 0 and $image != '') {
          $message[] = 'image name repeated!';
-      } elseif ($image_size > 2000000) {
+      } elseif ($image_size > 20000000) {
          $message[] = 'images size is too large!';
       } else {
          move_uploaded_file($image_tmp_name, $image_folder);
@@ -66,14 +66,14 @@ if (isset($_POST['publish'])) {
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-  
-  
+
+
    <link rel="stylesheet" href="./css/style.css">
-    <!-- font awesome cdn link  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
 
 </head>
@@ -81,32 +81,34 @@ if (isset($_POST['publish'])) {
 <body>
    <?php include 'header.php'; ?>
 
-<main class="container-fluid container-md pb-5">
-   <section class="post-editor">
+   <div class="container-fluid" style=" background-color: #f5f5f5;">
+      <section class="post-editor container-fluid container-md pb-5">
 
-      <h1 class="heading text-capitalize">Add new post</h1>
+         <h1 class="heading text-capitalize">Add new post</h1>
 
-      <form action="" method="post" enctype="multipart/form-data">
-         <input type="hidden" name="name" value="<?= $fetch_profile['name']; ?>">
-         <p>post caption <span>*</span></p>
-         <textarea name="caption" class="box" required maxlength="10000" placeholder="write your caption..." cols="30" rows="8"></textarea>
-         <p>post image <span>*</span></p>
-         <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png, image/webp">
-         <div class="publish-btn">
-            <input type="submit" value="Publish Post" name="publish" class="btn">
-         </div>
-      </form>
+         <form action="" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="name" value="<?= $fetch_profile['name']; ?>">
+            <p>post caption <span>*</span></p>
+            <textarea name="caption" class="box" required maxlength="10000" placeholder="write your caption..." cols="30" rows="8"></textarea>
+            <p>post image <span>*</span></p>
+            <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png, image/webp">
+            <div class="publish-btn">
+               <input type="submit" value="Publish Post" name="publish" class="btn">
+            </div>
+         </form>
 
-   </section>
+      </section>
 
-   </main>
+   </div>
+
+
 
 
 
 
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
 
 </body>
